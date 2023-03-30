dfb59f59bb6f356b60f392e7f981da4ebf2f0338
Bakdaulet <90865998+Aitbekov@users.noreply.github.com>
2023-03-30 12:49:55 +0600

    start point

---
+++ lib/udemy3.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+ int counter = 0;
+ void _incrementCounter() {
+   setState(() {
+     counter++;
+   });
+ }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+      appBar: AppBar(
+        title: Text('ART PROGRAMS'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text('$counter',
+          style: Theme.of(context).textTheme.headlineLarge,),
+        ),
+      ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            _incrementCounter();
+          },
+          tooltip: 'Increment',
+          child: Icon(Icons.add),
+        ),
+    ));
+    }
+}
+
+
+// class MyApp extends StatelessWidget {
+//
+//   @override
+//   Widget build(BuildContext context){
+//     return MaterialApp(
+//       home: Scaffold(
+//        appBar: AppBar(
+//          title: Text('ART PROGRAMS'),
+//        ),
+//         body: Container(
+//           child: Center(
+//             child: Text('StatelessWidget'),
+//           ),
+//         ),
+//
+//       ),
+//     );
+//   }
+// }
